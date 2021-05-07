7bfddf434650b26e358478d314499534bf2a3dc4
olteffe <olteffe@gmail.com>
2021-05-07 23:54:20 +0300

    add scrapy func for 1 page

--- scrappy.py
+++ scrappy.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 import settings
@@ -15,25 +16,29 @@ def scrappy_ad_info():
     with open("index.html", "r") as file:
         source = file.read()
     soup = BeautifulSoup(source, "lxml")
-    link_list, ad_name_list, author_list = [], [], []
+    link_list, ad_description_list, author_list = [], [], []
     raw_links = soup.find_all("a", class_="titleline")  # and raw_ad_name
     for link in raw_links:
         link_list.append(link.get("href"))
-    # print(link_list)
-    for ad_name in raw_links:
-        ad_name_list.append(ad_name.get_text(separator=" "))
-    # print(ad_name_list)
+    for ad_description in raw_links:
+        ad_description_list.append(re.sub(r'\s+', ' ', ad_description.get_text()).strip())
     raw_author = soup.find_all("td", class_="col-author")
     for author in raw_author:
-        author_list.append(author.get_text(separator=", "))
-    # print(author_list)
-    all_ad_list = list(zip(link_list, ad_name_list, author_list))
-    print(all_ad_list)
-    # TODO remove \n
+        author_list.append(re.sub(r'\s+', ' ', author.get_text(",")).strip())
+    all_ad_list = list(zip(link_list, ad_description_list, author_list))
 
 
-def check_pagination():
-    pass
+def check_pagination() -> int:
+    """return total number of pages"""
+    with open("index.html", "r") as file:
+        source = file.read()
+    soup = BeautifulSoup(source, "lxml")
+    soup_string = soup.find("div", class_="link_bar").get_text(separator=",", strip=True)
+    temp_list = list(soup_string.split(","))
+    if temp_list[-1] == 'следующая →':
+        return int(temp_list[-2])
+    elif int(temp_list[-1]) == 1:
+        return 1
 
 
 def scrappy_agency_number():
@@ -47,6 +52,7 @@ def save_in_db():
 if __name__ == '__main__':
     write_page_html()
     scrappy_ad_info()
+    # print(check_pagination())
     # list_links = []
     # scrappy_url_page()
     # print(list_links, len(list_links))
